02836f450d0992c693d6d908db23fa0e93f204b6
D1nggDong <82520542+D1nggDong@users.noreply.github.com>
2025-12-25 15:03:07 -0500

    Update keep_alive.py

--- Free-Game-Notifier/src/keep_alive.py
+++ Free-Game-Notifier/src/keep_alive.py
@@ -1,15 +1,1 @@
-from Flask import Flask
-from threading import Thread
 
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot is Running!"
-    
-def run():
-    app.run(host="0.0.0.0", port=1000)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
